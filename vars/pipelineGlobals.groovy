53bef8dc8b04679ccdae24a10cf91c435e83d0ad
Bulliraju <bulliraju118@gmail.com>
2026-04-02 13:06:48 +0530

    I updated jenkinsfile

--- vars/pipelineGlobals.groovy
+++ vars/pipelineGlobals.groovy
@@ -1,14 +1,14 @@
 def getAccountId(String environment) {
     switch (environment) {
-        case "dev":
+        case 'dev':
             return "017183880052"
-        case "qa":
+        case 'qa':
             return "017183880052"
-        case "uat":
+        case 'uat':
             return "017183880052"
-        case "pre-prod":
+        case 'pre-prod':
             return "017183880052"
-        case "prod":
+        case 'prod':
             return "017183880052"
         default:
             return "nothing"
